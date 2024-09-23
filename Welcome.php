26c7639cc669fa06a43a706c5952354353323539
Raghav140 <raghavsharma.jan2004@gmail.com>
2024-09-23 10:09:19 +0530

    Add files via upload
    Here we created a form where we added user name ,email and password with the help of user input

---
+++ Welcome.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>$_POST-to collect form data</title>
+</head>
+<body>
+welcome User:
+<?php
+echo $_POST["name"];
+?><br><br>
+your email address is:
+<?php
+echo $_POST["email"];
+?><br><br>
+Your password is:
+<?php
+echo $_POST["Password"];
+?><br><br>
+</body>
+</html>
